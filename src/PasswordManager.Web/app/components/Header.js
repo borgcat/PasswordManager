7200bf9e19ee19d178941d38f0456d264449713d
weswhite <wtealwhite@gmail.com>
2016-03-01 16:59:50 -0600

    bringing in FME file for web frontend \o/

---
+++ src/PasswordManager.Web/app/components/Header.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {Link} from 'react-router';
+import HeaderStore from '../stores/HeaderStore';
+import HeaderActions from '../actions/HeaderActions';
+
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        {/*this.state = HeaderStore.getState();*/}
+        this.onChange = this.onChange.bind(this);
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillUnmount() {
+        {/*HeaderStore.unlisten(this.onChange);*/}
+    }
+
+    onChange(state) {
+        this.setState(state);
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+    }
+
+    render() {
+        return (
+            <nav className="navbar navbar-default">
+                <div className="container">
+                    <div className="navbar-header">
+                        <div className="pull-left">
+                            <h2>Full Metal Engineering Password Manager</h2>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default Header;
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/PasswordManager.Web/app/main.js
@@ -0,0 +1,10 @@
+﻿import React from 'react';
+import Router from 'react-router';
+import ReactDOM from 'react-dom';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
+import routes from './routes';
+import Header from './components/Header';
+
+let history = createBrowserHistory();
+
+ReactDOM.render(<Router history={history}>{routes}</Router>, document.getElementById('app'));
