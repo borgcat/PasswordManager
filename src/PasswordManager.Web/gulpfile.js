b9ba1ae3f107dfa8b72e3a4ea6960ed3d763395d
weswhite <wtealwhite@gmail.com>
2016-03-03 14:37:45 -0600

    fix gulp build

--- src/PasswordManager.Web/gulpfile.js
+++ src/PasswordManager.Web/gulpfile.js
@@ -1,45 +1,127 @@
-﻿/// <binding Clean='clean' />
+﻿/// <binding Clean='clean, min' />
 
-var gulp = require("gulp"),
-    rimraf = require("rimraf"),
-    concat = require("gulp-concat"),
-    cssmin = require("gulp-cssmin"),
-    uglify = require("gulp-uglify"),
-    project = require("./project.json");
+var gulp = require("gulp");
+var rimraf = require("rimraf");
+var concat = require("gulp-concat");
+var cssmin = require("gulp-cssmin");
+var uglify = require("gulp-uglify");
+var gutil = require('gulp-util');
+var gulpif = require('gulp-if');
+var autoprefixer = require('gulp-autoprefixer');
+var less = require('gulp-less');
+var plumber = require('gulp-plumber');
+var buffer = require('vinyl-buffer');
+var source = require('vinyl-source-stream');
+var babelify = require('babelify');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var sourcemaps = require('gulp-sourcemaps');
+var lint = require('gulp-eslint');
+var project = require("./project.json");
 
 var paths = {
     webroot: "./" + project.webroot + "/"
 };
 
-paths.js = paths.webroot + "js/**/*.js";
-paths.minJs = paths.webroot + "js/**/*.min.js";
-paths.css = paths.webroot + "css/**/*.css";
-paths.minCss = paths.webroot + "css/**/*.min.css";
-paths.concatJsDest = paths.webroot + "js/site.min.js";
-paths.concatCssDest = paths.webroot + "css/site.min.css";
-
 gulp.task("clean:js", function (cb) {
-    rimraf(paths.concatJsDest, cb);
+    rimraf(paths.webroot + 'js/vendor.js');
+    rimraf(paths.webroot + 'js/vendor.bundle.js', cb);
+    //rimraf(paths.webroot + 'js/bundle.js', cb);
 });
 
 gulp.task("clean:css", function (cb) {
-    rimraf(paths.concatCssDest, cb);
+    rimraf(paths.webroot + 'css/**/*.css', cb);
 });
 
-gulp.task("clean", ["clean:js", "clean:css"]);
+gulp.task("clean", ['clean:js', 'clean:css']);
 
-gulp.task("min:js", function () {
-    gulp.src([paths.js, "!" + paths.minJs], { base: "." })
-        .pipe(concat(paths.concatJsDest))
-        .pipe(uglify())
-        .pipe(gulp.dest("."));
+var dependencies = [
+    'alt',
+    'react',
+    'react-dom',
+    'react-router',
+    'underscore'
+];
+
+gulp.task('vendor', function () {
+    return gulp.src([
+        paths.webroot + 'lib/jquery/dist/jquery.js',
+        paths.webroot + 'lib/bootstrap/dist/js/bootstrap.js',
+        paths.webroot + 'lib/magnific-popup/dist/jquery.magnific-popup.js',
+        paths.webroot + 'lib/toastr/toastr.js'
+    ])
+        .pipe(concat('vendor.js'))
+        .pipe(uglify({ mangle: false }))
+        .pipe(gulp.dest(paths.webroot + '/js'));
 });
 
-gulp.task("min:css", function () {
-    gulp.src([paths.css, "!" + paths.minCss])
-        .pipe(concat(paths.concatCssDest))
+gulp.task('browserify-vendor', function () {
+    return browserify()
+        .require(dependencies)
+        .bundle()
+        .pipe(source('vendor.bundle.js'))
+        .pipe(buffer())
+        .pipe(uglify({ mangle: false }))
+        .pipe(gulp.dest(paths.webroot + '/js'));
+});
+
+gulp.task('browserify', ['browserify-vendor'], function () {
+    return browserify({ entries: 'Client/app/main.js', debug: true })
+        .external(dependencies)
+        .transform(babelify, { presets: ['es2015', 'react'] })
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(uglify({ mangle: false }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(paths.webroot + '/js'));
+});
+
+//gulp.task('browserify-watch', ['browserify-vendor'], function () {
+//    var bundler = watchify(browserify({ entries: 'app/main.js', debug: true }, watchify.args));
+//    bundler.external(dependencies);
+//    bundler.transform(babelify, { presets: ['es2015', 'react'] });
+//    bundler.on('update', rebundle);
+//    return rebundle();
+
+//    function rebundle() {
+//        var start = Date.now();
+//        return bundler.bundle()
+//            .on('error', function (err) {
+//                gutil.log(gutil.colors.red(err.toString()));
+//            })
+//            .on('end', function () {
+//                gutil.log(gutil.colors.green('Finished rebundling in', (Date.now() - start) + 'ms.'));
+//            })
+//            .pipe(source('bundle.js'))
+//            .pipe(buffer())
+//            .pipe(sourcemaps.init({ loadMaps: true }))
+//            .pipe(sourcemaps.write('.'))
+//            .pipe(gulp.dest('public/js/'));
+//    }
+//});
+
+//still working on theses to port from node to .NET core
+gulp.task('styles', function () {
+    return gulp.src('Client/app/stylesheets/main.less')
+        .pipe(plumber())
+        .pipe(less())
+        .pipe(autoprefixer())
         .pipe(cssmin())
-        .pipe(gulp.dest("."));
+        .pipe(gulp.dest(paths.webroot + '/css'));
 });
 
-gulp.task("min", ["min:js", "min:css"]);
+gulp.task('lint', function () {
+    return gulp.src('bundle.js')
+        .pipe(lint({ config: 'eslint.config.json' }))
+        .pipe(lint.format());
+});
+
+
+gulp.task('watch', function () {
+    gulp.watch('app/stylesheets/**/*.less', ['styles']);
+});
+
+gulp.task('default', ['styles', 'vendor', 'browserify-watch', 'lint', 'watch']);
+gulp.task('build', ['styles', 'vendor', 'browserify']);
