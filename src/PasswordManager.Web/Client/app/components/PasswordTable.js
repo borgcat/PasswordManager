6efab98dde2f9e22da2c52086383335c1d111f08
Wes White <wtealwhite@gmail.com>
2016-03-12 11:16:58 -0600

    fixing react router

--- src/PasswordManager.Web/Client/app/components/PasswordTable.js
+++ src/PasswordManager.Web/Client/app/components/PasswordTable.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router';
+import PasswordTableStore from '../stores/PasswordTableStore'
+import PasswordTableActions from '../actions/PasswordTableActions';
 
 
 class PasswordTable extends React.Component {
