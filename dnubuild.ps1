1a2a906cdb9b5d455dec34ed8fef0cad567bee92
Dave Gardner <dave@texasgardners.com>
2016-03-15 09:31:45 -0500

    added exception handling to dnubuild

--- dnubuild.ps1
+++ dnubuild.ps1
@@ -1,6 +1,5 @@
 param(
-    [parameter(Mandatory=$true)] [string]$workdir,
-	[parameter(Mandatory=$true)] [string]$project,
+    [parameter(Mandatory=$true)] [string]$project,
     [parameter(Mandatory=$true)] [string]$buildConfiguration
 )
 
@@ -8,35 +7,40 @@ Write-Host '----------------------------------------'
 Write-Host '       Parameters'
 Write-Host '----------------------------------------'
 Write-Host 'PSScriptRoot' $PSScriptRoot
-Write-Host 'workdir' $workdir
 Write-Host 'project' $project
 Write-Host 'buildConfiguration' $buildConfiguration
 Write-Host '----------------------------------------'
 
- # bootstrap DNVM into this session.
-&{$branch;iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}
+try{
+	# bootstrap DNVM into this session.
+	&{$branch;iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}
 
-# load up the global.json so we can find the DNX version
-$globalJson = Get-Content -Path $PSScriptRoot\global.json -Raw -ErrorAction Ignore | ConvertFrom-Json -ErrorAction Ignore
+	# load up the global.json so we can find the DNX version
+	$globalJson = Get-Content -Path $PSScriptRoot\global.json -Raw -ErrorAction Ignore | ConvertFrom-Json -ErrorAction Ignore
 
-if($globalJson)
-{
-    $dnxVersion = $globalJson.sdk.version
+	if($globalJson)
+	{
+		$dnxVersion = $globalJson.sdk.version
+	}
+	else
+	{
+		Write-Warning "Unable to locate global.json to determine using 'latest'"
+		$dnxVersion = "latest"
+	}
+
+	& $env:USERPROFILE\.dnx\bin\dnvm install $dnxVersion -Persistent
+
+	# run DNU restore on all project.json files in the src folder including 2>1 to redirect stderr to stdout for badly behaved tools
+	Write-Host "===== RESTORE <-- done in Prebuild.ps1 ====="
+	Write-Host 'restoring project: ' $PSScriptRoot\$project\project.json
+	& dnu restore $PSScriptRoot\$project\project.json 
+		
+	# run DNU build on all project.json files in the src folder including 2>1 to redirect stderr to stdout for badly behaved tools
+	Write-Host "===== BUILD ====="
+	Write-Host 'Building project: ' $PSScriptRoot\$project\project.json
+	& dnu build $PSScriptRoot\$project\project.json --configuration "$buildConfiguration" 
 }
-else
-{
-    Write-Warning "Unable to locate global.json to determine using 'latest'"
-    $dnxVersion = "latest"
+catch{
+    "An error occurred during publish.`n{0}" -f $_.Exception.Message | Write-Error
+    exit 1
 }
-
-& $env:USERPROFILE\.dnx\bin\dnvm install $dnxVersion -Persistent
-
-# run DNU restore on all project.json files in the src folder including 2>1 to redirect stderr to stdout for badly behaved tools
-Write-Host "===== RESTORE <-- done in Prebuild.ps1 ====="
-Write-Host 'restoring project: ' $PSScriptRoot\$project\project.json
-& dnu restore $PSScriptRoot\$project\project.json 
-	
-# run DNU build on all project.json files in the src folder including 2>1 to redirect stderr to stdout for badly behaved tools
-Write-Host "===== BUILD ====="
-Write-Host 'Building project: ' $PSScriptRoot\$project\project.json
-& dnu build $PSScriptRoot\$project\project.json --configuration "$buildConfiguration" 
