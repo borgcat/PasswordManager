8bd705cde304a96bab5462e8c649e2647ac749ae
Wes White <wtealwhite@gmail.com>
2016-04-08 20:29:28 -0500

    updating the password table component

--- src/PasswordManager.Web/Client/app/components/PasswordTable.js
+++ src/PasswordManager.Web/Client/app/components/PasswordTable.js
@@ -1,92 +1,177 @@
 import React from 'react';
 import {Link} from 'react-router';
-import ajax from 'superagent';
-import PasswordTableStore from '../stores/PasswordTableStore'
-import PasswordTableActions from '../actions/PasswordTableActions';
+import $ from 'jquery';
 
 
-class PasswordTable extends React.Component {
-    constructor(props) {
-        super(props);
-        //this.state = PasswordTableStore.getState();
-        this.onChange = this.onChange.bind(this);
-    }
+// class PasswordTable extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         //this.state = PasswordTableStore.getState();
+//         //this.onChange = this.onChange.bind(this);
+//     }
     
-    componentWillMount() {
-    ajax.get('/Home/GetListOfPassword')
-        .end((error, response) => {
-            if (!error && response) {
-                this.setState({ passwords: response.body });
-            } else {
-                console.log('error', error);
-            }
-        }
-    );
-}
-    
-    componentDidMount() {
-        PasswordTableStore.listen(this.onChange);
-    }
+// }         
 
-    componentWillUnmount() {
-        PassowordTableStore.unlisten(this.onChange);
-    }
-
-    onChange(state){
-        this.setState(state);
-    }
-    render() {
+var Password = React.createClass({
+    render: function() {
         return (
-            <div className="container">
-                {/* Banner */}
-                <div className="row">
-                    <div className='alert alert-info'>
-                        Table Component
-                    </div>
-                </div>
-                <div>{this.props.passwords}</div>
-                
-                {/*{/* table 
-                <div className="row">
-                    <div className="table-responsive">
-                        <table className="table table-hover">
-                            <thead>
-                                <tr className="text-center">
-                                    <td></td>
-                                    <td><b>Account</b></td>
-                                    <td><b>Password</b></td>
-                                    <td><b>Email</b></td>
-                                    <td><b>Username</b></td>
-                                    <td></td>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr className="text-center">
-                                    <td>
-                                        <Link to={'/table/'}>
-                                            <button type='submit' className='btn btn-warning'>Details</button>
-                                        </Link>
-                                    </td>
-                                    <td>facebook.com</td>
-                                    <td>p@ssword1</td>
-                                    <td>email@gmail.com</td>
-                                    <td>user1</td>
-                                    <td>
-                                        <Link to={'/table/'}>
-                                            <button type='submit' className='btn btn-info'>Edit</button>
-                                        </Link>
-                                    </td>
-                                </tr>                                
-                            </tbody>
-                        </table>
-                    </div>
-                    <Link to={'/add/'}>
-                        <button type='submit' className='btn btn-primary pull-right'>Add Account</button>
+            <tr className="text-center">
+                <td>
+                    <Link to={'/table/'}>
+                        <button type='submit' className='btn btn-warning'>Details</button>
                     </Link>
-                </div>*/}
-            </div>
+                </td>
+                <td>{this.props.CommonName}}</td>
+                <td>{this.props.Url}</td>
+                <td>{this.props.UserName}</td>
+                <td>{this.props.Password}</td>
+                <td>
+                    <Link to={'/table/'}>
+                        <button className='btn btn-info'>Edit</button>
+                    </Link>
+                </td>
+            </tr>      
         );
     }
-}
+});
+
+var PasswordList = React.createClass({
+    render: function() {
+        var passwordNode = this.props.data.map(function(password) { 
+            return (
+              <Password key={password.Id} />
+            );
+        });
+        return (
+            <div className="row">
+                <div className="table-responsive">
+                    <table className="table table-hover">
+                        <thead>
+                            <tr className="text-center">
+                                <td></td>
+                                <td><b>Account</b></td>
+                                <td><b>Password</b></td>
+                                <td><b>Email</b></td>
+                                <td><b>Username</b></td>
+                                <td></td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {passwordNode}                          
+                        </tbody>
+                    </table>
+                </div>
+                    <button  className='btn btn-primary pull-right'>Add Account</button>
+            </div>        
+        );
+    }
+});
+
+var PasswordForm = React.createClass({
+    getInitialState: function() {
+        return {CommonName: '', Url: '', UserName: '', Password: '' };
+    },
+    handleNameChange: function(e) {
+        this.setState({CommonName: e.target.value});
+    },
+    handleUrlChange: function(e) {
+        this.setState({Url: e.target.value});
+    },
+    handleUserNameChange: function(e) {
+        this.setState({UserName: e.target.value});
+    },
+    handlePasswordChange: function(e) {
+        this.setState({Password: e.target.value});
+    },
+    handleSubmit: function(e) {
+        e.preventDefault();
+        var CommonName = this.state.CommonName.trim();
+        var Url = this.state.Url.trim();
+        var UserName = this.state.UserName.trim();
+        var Password = this.state.Password.trim();
+        if (!CommonName || !Url || !UserName || !Password) {
+        return;
+        }
+        this.props.onCommentSubmit({CommonName: CommonName, Url: Url, UserName: UserName, Password: Password});
+        this.setState({CommonName: '', Url: '', UserName: '', Password: ''});
+    },
+    render: function() {
+        return (
+        <form className="passwordForm" onSubmit={this.handleSubmit}>
+            <input
+            type="text"
+            placeholder="Common Name"
+            value={this.state.CommonName}
+            onChange={this.handleCommonNameChange}
+            />
+            <input
+            type="text"
+            placeholder="Url"
+            value={this.state.Url}
+            onChange={this.handleUrlChange}
+            />
+            <input
+            type="text"
+            placeholder="Username"
+            value={this.state.UserName}
+            onChange={this.handleUserNameChange}
+            />
+            <input
+            type="text"
+            placeholder="Password"
+            value={this.state.Password}
+            onChange={this.handlePasswordChange}
+            />
+            <input type="submit" value="Post" />
+        </form>
+        );
+    }
+});
+
+var PasswordTable = React.createClass({
+    loadCommentsFromServer: function() {
+        $.ajax({
+        url: this.props.api,
+        dataType: 'json',
+        cache: false,
+        success: function(data) {
+            this.setState({data: data});
+        }.bind(this),
+        error: function(xhr, status, err) {
+            console.error(this.props.url, status, err.toString());
+        }.bind(this)
+        });
+    },
+    handleCommentSubmit: function(password) {
+        $.ajax({
+        url: this.props.api,
+        dataType: 'json',
+        type: 'POST',
+        data: password,
+        success: function(data) {
+            this.setState({data: data});
+        }.bind(this),
+        error: function(xhr, status, err) {
+            console.error(this.props.url, status, err.toString());
+        }.bind(this)
+        });
+    },
+    getInitialState: function() {
+        return {data: []};
+    },
+    componentDidMount: function() {
+        this.loadCommentsFromServer();
+        setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+    },
+    render: function() {
+        return (
+        <div className="commentBox">
+            <h1>Passwords</h1>
+            <PasswordList data={this.state.data} />
+            <PasswordForm onCommentSubmit={this.handlePasswordSubmit} />
+        </div>
+        );
+    }
+});
 
 export default PasswordTable;
